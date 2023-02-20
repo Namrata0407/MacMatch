7cf8de34f6cbd3416d41ff61852edef91c95180a
Kausik Bhaumik <kausikbhaumik42@gmail.com>
2023-02-21 01:57:16 +0530

    started

--- project/src/App.js
+++ project/src/App.js
@@ -1,12 +1,7 @@
-
 // import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+  return <div className="App">our project</div>;
 }
 
 export default App;
